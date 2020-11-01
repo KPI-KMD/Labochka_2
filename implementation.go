415b726aaa03812819b83a91e5b024de61c963be
andreichenko256 <andreichenko.kirill2000@gmail.com>
2020-11-01 16:16:23 +0200

    Implemented postfix computation function in file implementation.go

--- implementation.go
+++ implementation.go
@@ -1,9 +1,59 @@
 package lab2
 
-import "fmt"
+import (
+	"errors"
+	"math"
+	"strconv"
+	"strings"
+)
 
-// TODO: document this function.
-// PrefixToPostfix converts
-func PrefixToPostfix(input string) (string, error) {
-	return "TODO", fmt.Errorf("TODO")
+func EvaluatePostfix(input string) (int, error) {
+
+	evaluateOperators := map[string]func(int, int) int{
+		"+": func(x, y int) int {
+			return x + y
+		},
+		"-": func(x, y int) int {
+			return x - y
+		},
+		"*": func(x, y int) int {
+			return x * y
+		},
+		"/": func(x, y int) int {
+			return x / y
+		},
+		"^": func(x, y int) int {
+			return int(math.Pow(float64(x), float64(y)))
+		},
+	}
+
+	if input == "" {
+		return 0, errors.New("Input can not be empty")
+	}
+
+	stringArray := strings.Fields(input)
+	var stack []int
+
+	for _, token := range stringArray {
+		if operators, consist := evaluateOperators[token]; consist {
+			if len(stack) < 2 {
+				return 0, errors.New("Missing operand")
+			}
+			arg1, arg2 := stack[len(stack)-2], stack[len(stack)-1]
+			stack = stack[:len(stack)-2]
+			stack = append(stack, operators(arg1, arg2))
+		} else {
+			val, err := strconv.Atoi(token)
+			if err != nil {
+				return 0, err
+			}
+			stack = append(stack, val)
+		}
+	}
+
+	if len(stack) != 1 {
+		return -1, errors.New("Stack corrupted")
+	}
+
+	return stack[len(stack)-1], nil
 }
