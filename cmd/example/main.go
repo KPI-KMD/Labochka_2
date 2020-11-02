177cce9910b2599bcaa20ee9558546534f3a5fb6
dimakryshtal <dima.krshl@gmail.com>
2020-11-02 21:04:57 +0200

    secondTask1

--- cmd/example/main.go
+++ cmd/example/main.go
@@ -2,28 +2,60 @@ package main
 
 import (
 	"flag"
-	"fmt"
-
+	"os"
+	"io"
+	"log"
+	"strings"
 	lab2 "github.com/KPI-KMD/Labochka_2"
 )
 
 var (
 	inputExpression = flag.String("e", "", "Expression to compute")
-	// TODO: Add other flags support for input and output configuration.
+	inputFile = flag.String("f","","File with expression")
+	resultFile = flag.String("o","","File with calculation result")
 )
 
 func main() {
 	flag.Parse()
+	var ans io.Reader
+	var wr io.Writer
+	var err error
+	
+	if (*inputFile == "" && *inputExpression == "") {
+		l := log.New(os.Stderr, "", 0)
+		l.Println("ERROR: USE -e to enter an expression to compute; \n\t   -f to enter location of a file containing an expression.")
+		os.Exit(3)
+	}
 
-	// TODO: Change this to accept input from the command line arguments as described in the task and
-	//       output the results using the ComputeHandler instance.
-	//       handler := &lab2.ComputeHandler{
-	//           Input: {construct io.Reader according the command line parameters},
-	//           Output: {construct io.Writer according the command line parameters},
-	//       }
-	//       err := handler.Compute()
+	if *inputFile != "" {
+		ans, err = os.Open(*inputFile);
+		if err != nil {
+			l := log.New(os.Stderr, "", 0)
+			l.Println("ERROR: ", err)
+			os.Exit(3)
+		}
+	} else {
+		ans = strings.NewReader(*inputExpression)
+	}
 
-	res, _ := lab2.EvaluatePostfix("+ 2 2")
-	fmt.Println(res)
+	if(*resultFile != "") {
+		wr, err = os.Create(*resultFile);
+		if err != nil {
+			l := log.New(os.Stderr, "", 0)
+			l.Println("ERROR: ", err)
+			os.Exit(3)
+		}
+	} 
 
+
+	handler := &lab2.ComputeHandler{
+	    Input: ans,
+	    Output: wr,	
+	}
+	err = handler.Compute()	
+	if err != nil {
+		l := log.New(os.Stderr, "", 0)
+		l.Println("ERROR: ", err)
+		os.Exit(3)
+	}
 }
